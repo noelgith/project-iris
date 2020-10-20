85590d0176218044f174cfb9cb563b6d9ceac580
NoelJP <noeljp98@gmail.com>
2020-10-20 08:42:34 +0530

    commit

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ admin.initializeApp();
 exports.addAdminRole = functions.https.onCall((data, context) => {
     // check request is made by an admin
     if (context.auth.token.admin !== true) {
-        return {error: 'only admins can add other admins, sucker'}
+        return {error: 'only admins can add other admins'}
     }
     
     // get user and custom (admin)
